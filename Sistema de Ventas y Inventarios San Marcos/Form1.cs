ab4da208016f740c93f7b744ef450d1bf41f7d16
Mark Miranda <mi410577@uaeh.edu.mx>
2025-03-27 17:36:59 -0600

    Agregar archivos de proyecto.

---
+++ Sistema de Ventas y Inventarios San Marcos/Form1.cs
@@ -0,0 +1,10 @@
+namespace Sistema_de_Ventas_y_Inventarios_San_Marcos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
